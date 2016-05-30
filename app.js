08b63cff21ac8be1e4d51980877d705aa587ecf2
Arnoid31 <arnaud.commelin@gmail.com>
2016-05-30 10:14:24 +0200

    Added main:app.js in package.json

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE')
         if (req.method === 'OPTIONS') return res.sendStatus(200)
     }
-    next()
-})
+    next();
+});
 
 
 
